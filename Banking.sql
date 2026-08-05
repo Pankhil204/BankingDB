c2b6e926b626b267aa267f977edaedc4ea3f716a
Pankhil204 <pankhilss0797@gmail.com>
2026-08-05 09:08:44 +0530

    Filtered Records using DQL

--- Banking.sql
+++ Banking.sql
@@ -158,6 +158,86 @@ UPDATE Customers
 SET AccountCreationDate = '2024-03-26'
 WHERE CustomerID = 101;
 
+INSERT INTO Customers
+VALUES
+(105,'Gaurav','Thakare','gaurav@gmail.com','9322241286','2022-12-14','2004-08-27');
+
+INSERT INTO Accounts
+(AccountID, CustomerID, AccountType, Balance)
+Values
+(202,102,'Current',50000),
+(203,103,'Savings',40000),
+(204,104,'Current',100000),
+(205,105,'Savings',85000);
+
+INSERT INTO Branches
+(BranchID, BranchName, BranchAddress, BranchPhone)
+VALUES
+(01,'Panchvati','Panchvati Karanja',2233556),
+(02,'Gangapur','Serene Meadows',2478523),
+(03,'College Campus','College Road',2789456),
+(04,'Indraprasth','Dindori Road',2258528),
+(05,'Makhamalabad','Makhamalabad Road',2963963);
+
+INSERT INTO Loans
+(LoanID, LoanAmount, InterestRate, StartDate, EndDate, CustommerID)
+VALUES
+(301,50000,8,'2025-06-01','2026-05-31',101),
+(302,89000,8,'2026-07-01','2027-06-30',105),
+(303,67000,8,'2025-01-01','2026-04-30',104),
+(304,55000,8,'2026-08-01','2028-08-31',102),
+(305,40000,8,'2025-02-01','2026-11-30',103);
+
+INSERT INTO Transactions
+VALUES
+(1,'2025-01-10',5000,'Deposit',201),
+(2,'2026-10-05',10000,'Return',202),
+(3,'2025-04-19',15000,'Deposit',203),
+(4,'2024-06-01',25000,'Return',204),
+(5,'2025-12-10',15000,'Deposit',205);
+
+DELETE FROM customers
+WHERE CustomerID = 101;
+
+UPDATE Accounts
+SET Balance = Balance + 5000
+WHERE CustomerID = 101;
+
+UPDATE Customers 
+set 
+	email = 'rahulsharma@gmail.com',
+	MobileNo = '7884455112'
+WHERE 
+		CustomerID = 101;
+ 
+SELECT
+	CustomerID, FirstName, LastName, MobileNo
+From
+	Customers;
+
+SELECT * FROM Accounts
+WHERE Balance > 30000;
+
+SELECT * FROM Accounts
+WHERE AccountType = 'Savings' ;
+
+SELECT * FROM Accounts
+WHERE AccountType != 'Savings' ;
+
+SELECT * FROM Accounts
+WHERE AccountType <> 'Savings' ;
+
+SELECT * FROM Accounts
+WHERE Balance >= 30000 AND AccountType = 'Savings' ;
+
+SELECT * FROM Accounts
+WHERE Balance >= 30000 OR AccountType = 'Savings' ; -- use OR or || in syntax
+
+SELECT * from Accounts
+WHERE NOT AccountType = 'Savings';
+
+SELECT * FROM Accounts
+WHERE Balance >= 30000 && AccountType = 'Savings' ; -- use AND o && in Syntax 
 
 select * from customers;
 select * from Accounts;
